a15e21103b06701c3dadd6246f9d3ec8710fec2f
Rico van der Velde <524444@student.glu.nl>
2020-11-17 14:03:30 +0100

    zombie
    added zombie that looks for a target

---
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyAI : MonoBehaviour
+{
+    NavMeshAgent Agent;
+
+    [SerializeField] Transform target;
+
+    [SerializeField] enum AIState {Idle, Chaising};
+
+    [SerializeField] AIState aiState = AIState.Idle;
+
+    void Start()
+    {
+        Agent = GetComponent<NavMeshAgent>();
+        StartCoroutine(States());
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator States()
+    {
+        while (true)
+        {
+            switch (aiState)
+            {
+                case AIState.Idle:
+                    break;
+                case AIState.Chaising:
+                    Agent.SetDestination(target.position);
+                    break;
+                default:
+                    break;
+            }
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
