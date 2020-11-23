c92ba1461cbc17d0c869100d239973ebb70ab36d
Luke de Klein <delukeklein@gmail.com>
2020-11-23 13:35:25 +0100

    Revert "Remove things"
    This reverts commit bf31736c1fe8d94a84e3f768534b8767fa845540.

---
+++ Assets/Scripts/Weapon/WeaponData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Weapon Data", fileName = "Weapon Data")]
+public class WeaponData : ScriptableObject
+{
+    public Mesh mesh;
+
+    public uint Damage;
+
+    public uint Recoil;
+
+    public uint FireRate;
+}
