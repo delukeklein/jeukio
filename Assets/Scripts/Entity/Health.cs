20ca73c752597bfc7de5c79de7d342efc5c485e5
Luke de Klein <delukeklein@gmail.com>
2020-11-23 12:00:47 +0100

    Health

---
+++ Assets/Scripts/Entity/Health.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private int health;
+
+    [SerializeField] private int maxHealth;
+
+    public void Heal(uint amount) => health = (int) Mathf.Clamp(health + amount, 0, maxHealth);
+
+    public void Reduce(uint amount) => health -= (int) amount;
+
+    private void Start() => health = health = (int)Mathf.Clamp(health, 0, maxHealth);
+}
