bf31736c1fe8d94a84e3f768534b8767fa845540
Luke de Klein <delukeklein@gmail.com>
2020-11-23 13:35:18 +0100

    Remove things

---
+++ Assets/Scripts/Entity/Player/PointController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PointsHolder : MonoBehaviour
+{
+    private int points;
+
+    public void Add(uint amount) => points += amount;
+
+    public bool Reduce(uint amount)
+    {
+        int points = this.points - amount;
+
+        if(points >= 0)
+        {
+            this.points = points;
+
+            return true;
+        }
+
+        return false;
+    }
+}
