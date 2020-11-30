9c54cf722d31291b18157b450eeedc87b3395cc8
Luke de Klein <delukeklein@gmail.com>
2020-11-30 11:25:18 +0100

    EnemyHit

---
+++ Assets/Scripts/Entity/Enemy/EnemyHit.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Health))]
+public class EnemyHit : MonoBehaviour
+{
+    private Health health;
+
+    private void Start() => health = GetComponent<Health>();
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        BulletScript bullet;
+
+        if(collision.gameObject.TryGetComponent(out bullet)
+        {
+            health.Reduce(25);
+        }
+    }
+}
