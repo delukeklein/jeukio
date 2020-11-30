fbdc197c9824f8703b110e25807bb4f0fc92ca9c
Luke de Klein <delukeklein@gmail.com>
2020-11-30 10:14:17 +0100

    Point Controller

---
+++ Assets/Scripts/Entity/Player/PointController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PointsHolder : MonoBehaviour
+{
+    private int points;
+
+    public void Add(uint amount) => points += (int) amount;
+
+    public bool Reduce(uint amount)
+    {
+        int points = this.points - (int) amount;
+
+        if(points >= 0)
+        {
+            this.points = points;
+
+            return true;
+        }
+
+        return false;
+    }
+}
