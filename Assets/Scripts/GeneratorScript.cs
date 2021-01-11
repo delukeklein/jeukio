689881580890be34c6836aaf3ff344df1f10fda8
Rico van der Velde <524444@student.glu.nl>
2021-01-11 15:02:59 +0100

    qweqwewqeqe

---
+++ Assets/Scripts/GeneratorScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratorScript : MonoBehaviour
+{
+    [Header("GO")]
+
+    GameObject Generator1;
+    GameObject Generator2;
+
+    [Header("Boollean")]
+
+    bool Generator1State = false;
+    bool Generator2State = false;
+
+    //[Header("ECT")]
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Generator1 && Generator1State && Generator2 && Generator2State == true)
+        {
+            //hier moeten de machines aan en de ui aan en de andere ui uit met (requires power) 
+        }
+    }
+}
