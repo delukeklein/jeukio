14d6a59481cbd489e728c6bed82becac28727c54
Luke de Klein <delukeklein@gmail.com>
2021-01-12 11:42:52 +0100

    Player Movement

---
+++ Assets/Scripts/Items/_un.cs
@@ -0,0 +1,36 @@
+﻿using DesertStormZombies.Utility;
+
+using UnityEngine;
+
+namespace DesertStormZombies.Items
+{
+    public class Gun : Weapon
+    {
+        [SerializeField] private int damage;
+        [SerializeField] private int reloadSpeed;
+
+        [SerializeField] private float fireRate;
+
+        [SerializeField] Transform shootingPoint;
+
+        private IntervalTimer intervalTimer;
+
+        protected override void Start()
+        {
+            intervalTimer = new IntervalTimer(fireRate);
+        }
+
+        public override void Attack()
+        {
+            if(intervalTimer.Check(Time.deltaTime))
+            {
+                //raycast
+            }
+        }
+
+        public void Reload()
+        {
+            //reload;
+        }
+    }
+}
