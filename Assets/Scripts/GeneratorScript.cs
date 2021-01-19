f3f09f07223bb52ceb9157168ba9e609909deda4
Rico van der Velde <524444@student.glu.nl>
2021-01-19 13:01:59 +0100

    Revert "ewrewrw"
    This reverts commit c0a7665984c382add346a485c9b5c7de41cb3b36.

--- Assets/Scripts/GeneratorScript.cs
+++ Assets/Scripts/GeneratorScript.cs
@@ -1,77 +1,33 @@
-﻿using DesertStormZombies.Entity.Player;
-using DesertStormZombies.Game;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-namespace DesertStormZombies.Interaction
+public class GeneratorScript : MonoBehaviour
 {
-    public class GeneratorScript : Interactable
+    [Header("GO")]
+
+    GameObject Generator1;
+    GameObject Generator2;
+
+    [Header("Boollean")]
+
+    bool Generator1State = false;
+    bool Generator2State = false;
+
+    //[Header("ECT")]
+
+
+
+    void Start()
     {
-        public override void Focused(PlayerInteractor interactor)
+        
+    }
+
+    void Update()
+    {
+        if(Generator1 && Generator1State && Generator2 && Generator2State == true)
         {
-            interactor.SetText("Press E to Active");
-
-        }
-
-        public override void Interact(PlayerInteractor interactor)
-        {
-            Collider.enabled = false;
-
-
-            Generator1Active = true;
-
-            if (Generator1Active && Generator2Active == true)
-            {
-                FastReloadActive.SetActive(true);
-                RapidFireActive.SetActive(true);
-                HealthUpActive.SetActive(true);
-                SpeedOActive.SetActive(true);
-            }
-
-        }
-
-        public override bool Condition(PlayerInteractor interactor)
-        {
-            return true;
-        }
-
-        [Header("Generators")]
-
-        [SerializeField] GameObject Generator1;
-        [SerializeField] GameObject Generator2;
-
-        [Header("PowerUps")]
-
-        [SerializeField] GameObject FastReloadActive;
-        [SerializeField] GameObject RapidFireActive;
-        [SerializeField] GameObject HealthUpActive;
-        [SerializeField] GameObject SpeedOActive;
-
-
-
-        [Header("Boollean")]
-        [SerializeField] bool Generator1Active = false;
-        [SerializeField] bool Generator2Active = false;
-
-        //[Header("ECT")]
-
-
-
-        protected override void Start()
-        {
-            base.Start();
-
-            FastReloadActive.SetActive(false);
-            RapidFireActive.SetActive(false);
-            HealthUpActive.SetActive(false);
-            SpeedOActive.SetActive(false);
-        }
-
-        void Update()
-        {
-
+            //hier moeten de machines aan en de ui aan en de andere ui uit met (requires power) 
         }
     }
 }
